5ba095c73e45f73ec31e7a958bcc65118561fcdb
Luxgnite <ewan.dubois@live.fr>
2020-12-09 15:55:36 +0100

    Importation du code de base pour la State Machine

---
+++ Assets/Scripts/StateMachine/Turn States/TurnState.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnState : State
+{
+    //protected TurnController owner;
+
+    protected virtual void Awake()
+    {
+        //owner = GetComponent<TurnController>();
+    }
+
+    protected override void AddListeners()
+    {
+        Debug.Log("Adding Listeners");
+        //InputController.fireEvent += OnFire;
+
+    }
+
+    protected override void RemoveListeners()
+    {
+        Debug.Log("Removing Listeners");
+        //InputController.fireEvent -= OnFire;
+    }
+
+    /*
+    protected virtual void OnEvent()
+    {
+
+    }
+    */
+
+}
