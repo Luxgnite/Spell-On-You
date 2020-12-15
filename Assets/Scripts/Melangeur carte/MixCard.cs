ee939d548daf35a3306235bb1ae48e38cc7798fb
LPIZZIO <laura.pizzio@gmail.com>
2020-12-15 16:09:34 +0100

    new script MixCard

---
+++ Assets/Scripts/Melangeur carte/MixCard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using unityEngine.UI;
+
+
+public class MixCard : MonoBehaviour
+{ 
+
+    public void Mix()
+    {
+        // Insert cards at random order into the shuffled list 
+        var Mix = new List<Card>();
+        var rand = new Random();
+
+        // As long as there are any cards left to insert randomly 
+        while (MixCard.Count != 0)
+        {
+            // Get the index of the random card to insert 
+            var i = rand.Next(MixCard.Count);
+
+            // Insert it 
+            Mixed.Add(kaarten[i]);
+
+            // Remove from non-shuffled list 
+            MixCard.RemoveAt(i);
+        }
+
+        // Set the list of cards to the shuffled list 
+        MixCard = Mixed;
+    }
+}
